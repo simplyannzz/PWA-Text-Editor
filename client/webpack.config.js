64a590f79529f0664ca95805b0f6e5a08edadf42
Annie <anniebui0110@gmil.com>
2023-10-23 20:50:34 -0500

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,28 +3,73 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+//Add and configure workbox plugins for a service worker and manifest file.
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name}.bundle.js",
+      path: path.resolve(_dirname, "dist"),
     },
     plugins: [
-      
+      //Webpack to generates HTML files and inject nundles
+      newHtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Just another Text Editor",
+      }),
+      // Custom Service Worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      //Manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "Create notes with or without internet connection!",
+        background_color: "#272822",
+        theme_color: "#272822",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
-
+    // Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        // CSS loader to webpack
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        // Babel to webpack
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
-  };
+  }
 };
