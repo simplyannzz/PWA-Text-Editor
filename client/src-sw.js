cfed7d6dd9d981c68d7ca549fd12be5645a0ca64
Annie <anniebui0110@gmil.com>
2023-10-25 13:31:28 -0500

    sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,5 +26,17 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+//Implement asset caching
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of cache storage
+    cacheName: "assest-cache",
+    plugins: [
+      // plugin will cache responses with headers
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+)
